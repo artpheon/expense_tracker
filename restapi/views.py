b3a0e756d26ddf3fe981b5769acfb729cb13bed5
Artur N <idcdtokms@gmail.com>
2021-10-05 22:17:12 +0300

    added serializers

--- restapi/views.py
+++ restapi/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from restapi import models
+from restapi import models, serializers
 from django.forms.models import model_to_dict
 
 # Create your views here.
@@ -10,16 +10,12 @@ from django.forms.models import model_to_dict
 class ExpenseListCreate(APIView):
     def get(self, request):
         expenses = models.Expense.objects.all()
-        all_expenses = [model_to_dict(expense) for expense in expenses]
-        return Response(all_expenses, status=200)
+        serializer = serializers.Expense(expenses, many=True)
+        return Response(serializer.data, status=200)
 
     def post(self, request):
-        amount = request.data["amount"]
-        merchant = request.data["merchant"]
-        description = request.data["description"]
-        category = request.data["category"]
-        entry = models.Expense.objects.create(
-            amount=amount, merchant=merchant, description=description, category=category
-        )
+        serializer = serializers.Expense(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
 
-        return Response(model_to_dict(entry), status=201)
+        return Response(serializer.data, status=201)
