e93628c7d537c79efec03c36dcfecb2722cb30dc
artpheon <idcdtokms@gmail.com>
2021-11-02 22:58:28 +0300

    added filtering

--- restapi/tests.py
+++ restapi/tests.py
@@ -89,3 +89,23 @@ class TestViews(TestCase):
 
         self.assertEqual(204, res.status_code)
         self.assertFalse(models.Expense.objects.filter(pk=expense.id).exists())
+
+    def test_filter_by_merchnt(self):
+        amazon_expense = models.Expense.objects.create(
+            amount=1000, merchant="amazon", description="sunglasses", category="fashion"
+        )
+        ebay_expense = models.Expense.objects.create(
+            amount=2000, merchant="ebay", description="watch", category="fashion"
+        )
+
+        url = "/api/expenses?merchant=amazon"
+        res = self.client.get(url, format="json")
+
+        self.assertEqual(200, res.status_code)
+        json_res = res.json()
+        self.assertEqual(1, len(json_res))
+        self.assertEqual(amazon_expense.id, json_res[0]["id"])
+        self.assertEqual(amazon_expense.merchant, json_res[0]["merchant"])
+        self.assertEqual(amazon_expense.amount, json_res[0]["amount"])
+        self.assertEqual(amazon_expense.description, json_res[0]["description"])
+        self.assertEqual(amazon_expense.category, json_res[0]["category"])
